efc9c8e65dec593a0c6b230749d2369e458187ee
Niraj Khadka <khadka.niraj11111@gmail.com>
2025-07-05 13:14:11 +0000

    feature: calculator and temperature converted progran

---
+++ week-1/calculator.py
@@ -0,0 +1,18 @@
+print("Enter the first number: ");
+num1 = int(input());
+
+print("Enter the second number: ");
+num2 = int(input());
+
+print("Addition Result:", num1 + num2);
+
+print("Subtraction Result:", num1 - num2);
+
+print("Multiplication Result:", num1 * num2);
+
+print("Division Result:", num1 / num2);
+
+print("Modulus Result:", num1 % num2);
+
+print("Exponent Result:", num1 ** num2);
+
---
+++ week-1/temperature-converter.py
@@ -0,0 +1,4 @@
+print('Enter the temperature in Celsius: ');
+temperature = int(input());
+
+print('Output in Fahrenheit: ', temperature * 1.8 + 32);
